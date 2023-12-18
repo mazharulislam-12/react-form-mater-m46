11f9812dda7b4f88f4c448255ae44dd6842249ae
Mazharul Islam <505mazharulislam@gmail.com>
2023-12-18 14:23:09 +0600

    react intall

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import SimpleFrom from './components/SimpleForm/SimpleFrom'
+
+function App() {
+
+  return (
+    <>
+     
+      <h2>Simple Form for React</h2>
+      <SimpleFrom></SimpleFrom>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/components/SimpleForm/SimpleFrom.jsx
@@ -0,0 +1,12 @@
+
+const SimpleFrom = () => {
+    return (
+        <div>
+            <input type="text" />
+            <br />
+            <button>Submit</button>
+        </div>
+    );
+};
+
+export default SimpleFrom;
