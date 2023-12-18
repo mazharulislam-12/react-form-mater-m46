393f58a50169704be89ea3f19ba39587446995ae
Mazharul Islam <505mazharulislam@gmail.com>
2023-12-18 14:37:46 +0600

    form to data console

--- src/components/SimpleForm/SimpleFrom.jsx
+++ src/components/SimpleForm/SimpleFrom.jsx
@@ -1,10 +1,26 @@
 
 const SimpleFrom = () => {
+
+    const handleSubmti = e => {
+        e.preventDefault();
+        console.log(e.target.name.value);
+        console.log(e.target.email.value);
+        console.log(e.target.phone.value);
+        console.log('form submit');
+    }
+
     return (
         <div>
-            <input type="text" />
-            <br />
-            <button>Submit</button>
+            <form onSubmit={handleSubmti}>
+                <input type="text" name="name" />
+                <br />
+                <input type="text" name="email" />
+                <br />
+                <input type="text" name="phone" />
+                <br />
+                <input type="submit" value="Submit" />
+            </form>
+            
         </div>
     );
 };
